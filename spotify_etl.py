afe02a1c9da569a3a05f855bfb19545dafea2e69
StevenLomon <steven.lennartsson@gmail.com>
2024-04-14 10:26:42 +0200

    We have two functions and a dataframe

--- spotify_etl.py
+++ spotify_etl.py
@@ -1,5 +1,7 @@
 import os, requests, json, s3fs, re
 import pandas as pd
+from dotenv import load_dotenv
+from rich import print, print_json
 
 # df = pd.read_csv('spotify_songs.csv')
 
@@ -15,15 +17,16 @@ import pandas as pd
 
 # spotify_top_50 = df_final.to_csv('spotify-top-50.csv', index=False)
 
-X_RAPID_API_KEY = 
+load_dotenv()
+rapid_api_key = os.getenv("X_RAPID_API_KEY")
 
-def get_spotify_top_playlist_result(query:str) -> str:
+def get_spotify_top_playlist_result(query:str, api_key) -> str:
     url = "https://spotify23.p.rapidapi.com/search/"
 
     querystring = {"q":query,"type":"playlists","offset":"0","limit":"10","numberOfTopResults":"5"}
 
     headers = {
-        "X-RapidAPI-Key": "614be38f4bmshf29cb3db90ab80dp12c424jsn4c4279b56ddf",
+        "X-RapidAPI-Key": api_key,
         "X-RapidAPI-Host": "spotify23.p.rapidapi.com"
     }
 
@@ -31,21 +34,41 @@ def get_spotify_top_playlist_result(query:str) -> str:
 
     if response.status_code == 200:
         data = response.json()
-        playlist_uri = data.get('playlists', {}).get('items')[0].get('data').get('uri')
+        playlist_uri = data.get('playlists', {}).get('items', [{}])[0].get('data', {}).get('uri', None)
         playlist_id = re.search(r'playlist:(\S+)', playlist_uri).group(1)
     
     return playlist_id
 
-spotify_global_top_50_playlist_id = get_spotify_top_playlist_result('global top 50')
+spotify_global_top_50_playlist_id = get_spotify_top_playlist_result('global top 50', rapid_api_key)
 
-def get_playlist_tracks(playlist_id:str) -> dict:
+def get_playlist_tracks(playlist_id:str, api_key) -> list[dict]:
     url = "https://spotify23.p.rapidapi.com/playlist_tracks/"
 
     querystring = {"id":playlist_id,"offset":"0","limit":"50"}
 
     headers = {
-        "X-RapidAPI-Key": "614be38f4bmshf29cb3db90ab80dp12c424jsn4c4279b56ddf",
+        "X-RapidAPI-Key": api_key,
         "X-RapidAPI-Host": "spotify23.p.rapidapi.com"
     }
 
     response = requests.get(url, headers=headers, params=querystring)
+
+    song_list = []
+    if response.status_code == 200:
+        data = response.json()
+        items = data.get('items', [{}])
+        for item in items:
+            track = item.get('track', {})
+            song = {'id': track.get('id', None),
+                    'title': track.get('name', None),
+                    'artist': track.get('artists', [{}])[0].get('name', None),
+                    'popularity': track.get('popularity', None)}
+            song_list.append(song)
+    
+    return song_list
+
+spotify_global_top_50 = get_playlist_tracks(spotify_global_top_50_playlist_id, rapid_api_key)
+df = pd.DataFrame(spotify_global_top_50)
+df.sort_values("popularity", ascending=False, inplace=True)
+df.reset_index(inplace=True, drop=True)
+df.to_csv('spotify_global_top_50.csv', index=False)
